3e5f98d6962f07a38e5ae2d775d95d6ed76df2c3
ThaoHuynh <flashvnn@gmail.com>
2017-03-19 11:12:03 +0700

    Add IP, Port of Openshift for Total.js

--- release.js
+++ release.js
@@ -6,9 +6,8 @@
 
 var fs = require('fs');
 var options = {};
-
-// options.ip = '127.0.0.1';
-// options.port = parseInt(process.argv[2]);
+options.ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+options.port = parseInt(process.env.OPENSHIFT_NODEJS_PORT) || 8000;
 // options.config = { name: 'total.js' };
 // options.https = { key: fs.readFileSync('keys/agent2-key.pem'), cert: fs.readFileSync('keys/agent2-cert.pem')};
 // options.sleep = 2000;
